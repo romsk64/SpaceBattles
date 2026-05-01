c9cc6715eb6e3030d9bf018cb4b0c5892951500a
romsk64 <roma08shm@ya.ru>
2026-05-01 13:58:43 +0300

    moved rompgl.py from libs/ to main/

--- main/menu.py
+++ main/menu.py
@@ -1,4 +1,5 @@
 import pygame
+import rompgl
 
 pygame.init()
 
@@ -6,10 +7,13 @@ mw = pygame.display.set_mode((1250, 720))
 mainCycle_ = True
 
 while mainCycle_:
-    pygame.display.update()
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             mainCycle_ = False
             break
+    
+    # if exit_:
+    #     break
+    
+    pygame.display.update()
